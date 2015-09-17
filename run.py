c10a6298b8dcde05cab8d3705989327e3030c96c
djtyree <djtyree@alumni.clemson.edu>
2015-09-17 12:20:05 -0400

    used gevent instead of built in flask server.

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 #!flask/bin/python
+from gevent.wsgi import WSGIServer
 from main import app, manager
 import config
 
 @manager.command
 def runserver():
     print "Kicking off the server on localhost: " + str(config.PORT)
-    app.run(host='0.0.0.0', debug=config.DEBUG)
-
+    #app.run(host='0.0.0.0', debug=config.DEBUG)
+    server = WSGIServer(("", 5000), app)
+    server.serve_forever()
+    
 if __name__ == '__main__':
     manager.run()
+    
     
